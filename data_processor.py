66068046ac39916c4f7154c5cb2afc347ea422c1
XiaoMing <henyan94@gmail.com>
2017-08-16 12:20:22 +0200

    Added some more preparations in the data processor

--- data_processor.py
+++ data_processor.py
@@ -5,10 +5,19 @@ import os
 datadirpath = os.path.join(".", "data")
 
 def prerun():
+	ready = True
+	notReady = False
 	if not os.path.exists(datadirpath):
 		print("Missing data dir \n creating")
 		os.makedirs(datadirpath)
+	if os.listdir(datadirpath) == []:
+		print("Your data directory is empty")
+		return notReady
+
+ready = prerun()
+if not ready:
+	print("Not ready to run")
+	sys.exit()
 
 
-prerun()
 print("==== Script Done ====")
