e462dd82f26cc20a61dbc5dfca9b748304e4f803
Henry yang <henyan94@gmail.com>
2017-10-03 19:45:12 +0200

    add methods for reading, processing and generate data

--- data_processor.py
+++ data_processor.py
@@ -1,8 +1,10 @@
 import numpy as np
 import sqlite3
 import os
+import random
 
 datadirpath = os.path.join(".", "data")
+dataset = "english"
 
 def prerun():
 	ready = True
@@ -13,11 +15,35 @@ def prerun():
 	if os.listdir(datadirpath) == []:
 		print("Your data directory is empty")
 		return notReady
+	return ready
+
+def process_data(dataset):
+	data_path = os.path.join(datadirpath, dataset + ".txt")
+	datafile = open(data_path)
+	data_arr = datafile.read().split("\n")
+	return data_arr
+
+def rand_words(data_arr,lower,higher,count):
+	letters = set()
+	[[letters.add(c) for c in word] for word in data_arr]
+	letters = sorted(list(letters))
+	rand_sequences = []
+	word_lengths = np.random.randint(low=lower,high=higher,size=count)
+	for length in word_lengths:
+		sequence = [random.choice(letters) for i in range(length)]
+		sequence = "".join(sequence)
+		rand_sequences.append(sequence)
+
+	return rand_sequences
 
 ready = prerun()
 if not ready:
 	print("Not ready to run")
 	sys.exit()
+
+readable = process_data("english")
+not_readable = rand_words(data,1,20,len(readable))
+
 
 
 print("==== Script Done ====")
